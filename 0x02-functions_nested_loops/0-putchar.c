34d4db0f40f9563b86abea6398d4d0d7d82ec55b
BrianKani <kipsangkangogo26@gmail.com>
2023-02-21 04:06:24 -0800

    adding 1-alphabet.c file. It works!

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,19 @@
-#include <stdio.h>
-#include <main.h>
-main()
-{
-  int i=0;
-  if(i<5)
-    return "_putchar";
-    return 0;
+#include "main.h"
 
-}
+/**
+ * main - Prints Holberton as a message.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
