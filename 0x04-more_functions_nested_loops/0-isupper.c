e2bda8c1b257cabc1196599110785025041fe4c0
BrianKani <kipsangkangogo26@gmail.com>
2023-02-23 10:44:48 -0800

    Correcting my code to comply with betty

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,10 @@
-#include "main"
+#include "main.h"
 
 /**
  * _isupper - checks if parameter is an uppercase character.
  * @c: input character.
  * Return: 1 if is an uppercase character, 0 in other case.
  */
-
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
