3c3ba6cdd53712dfe5d6cf07ab73f398241ac474
BrianKani <kipsangkangogo26@gmail.com>
2023-03-06 11:17:16 -0800

    Fixing  the code.it works

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,17 @@
 #include "main.h"
-/*
- * This function takes in a pointer s to the memory location that needs to be filled, a char value b that represents the constant byte to be written to memory, and an unsigned int value n that represents the number of bytes to fill.
-*/
-
-char *_memset(char *s, char b, unsigned int n) {
-	for (unsigned int i = 0; i < n; i++) 
+/**
+ * _memset - fills memory with a constant byte,
+ * @s: memory area.
+ * @b: constant byte.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
+ */
+char *_memset(char *s, char b, unsigned int n)
 {
-		 s[i] = b;
-}
-	return s;
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) =  b;
+
+	return (s);
 }
