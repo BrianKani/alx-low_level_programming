966fd4160bcb84707e6bdf1d524986e842b6d6bd
BrianKani <kipsangkangogo26@gmail.com>
2023-03-16 06:45:57 -0700

    Checker error

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,16 +5,13 @@
  * @b: amount of bytes.
  *  Return: pointer to the allocated memory.
  */
-
-void *malloc_checked(unsigned int b);
+void *malloc_checked(unsigned int b)
 {
-
 	char *p;
 
 	p = malloc(b);
 	if (p == NULL)
 		exit(98);
 	return (p);
-
 }
 
