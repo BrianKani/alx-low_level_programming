c834b72c20e6aa3f3d7499e28a2ac105cc13d7ac
BrianKani <kipsangkangogo26@gmail.com>
2023-03-23 05:45:56 -0700

    Commenting for betty style

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 
 /**
- * int sum_them_all - calculates sum of all parameters
+ * sum_them_all - returns the sum of all its parameters.
  * @n: amount of the arguments.
  * Return: sum of its parameters.
  */
